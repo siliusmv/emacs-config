e5793cef25bee4d4cd9b68a21e790ccbb566ed1d
Silius M. Vandeskog <siliusv@gmail.com>
2019-09-01 12:25:44 +0200

    Remove dashboard, company-box, solaire-mode

--- init.el
+++ init.el
@@ -1463,13 +1463,21 @@
   )
 
 
-(use-package org-bullets
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+;; (use-package org-bullets
+;;   :config
+;;   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
+;; =========================================================================
+;; Modeline
+;; =========================================================================
 
 
 (use-package minions
   :config (minions-mode 1))
+
+
+(use-package all-the-icons :config (setq all-the-icons-scale-factor 1.0))
 
 (use-package fancy-battery
   :config
@@ -1636,103 +1644,6 @@ If DEFAULT is non-nil, set the default mode-line for all buffers."
 (require 'iso-transl)
 
 
-(use-package all-the-icons :config (setq all-the-icons-scale-factor 1.0))
-
-;;(use-package page-break-lines)
-
-;; (use-package dashboard
-;;   :config
-;;   (dashboard-setup-startup-hook)
-;;   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-;;   ;; To disable shortcut "jump" indicators for each section, set
-;;   (setq dashboard-show-shortcuts nil)
-;;   (setq dashboard-items '((projects  . 5)
-;; 			  ;;(bookmarks . 5)
-;; 			  (recents . 5)
-;; 			  ;;(agenda . 5)
-;; 			  ;;(registers . 5)
-;; 			  ))
-;;   (setq dashboard-set-heading-icons t)
-;;   (setq dashboard-set-file-icons t)
-;;   (setq dashboard-startup-banner 'logo)
-;;   (setq dashboard-banner-logo-title "Welcome back!")
-;;   )
-
-;; ;; (use-package company-quickhelp
-;; ;;   :hook (global-company-mode . company-quickhelp-mode)
-;; ;;   :init (setq company-quickhelp-delay 0.5)
-;; ;;   )
-;; 
-;; ;; (use-package company-box
-;; ;;   :hook (company-mode . company-box-mode)
-;; ;; 
-;; ;;   :config
-;; ;; 
-;; ;;   ;; https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-company.el#L76
-;; ;;   (when (and (display-graphic-p)
-;; ;; 	     (require 'all-the-icons nil t))
-;; ;;     (declare-function all-the-icons-faicon 'all-the-icons)
-;; ;;     (declare-function all-the-icons-material 'all-the-icons)
-;; ;;     (setq company-box-icons-all-the-icons
-;; ;; 	  `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (Text . ,(all-the-icons-faicon "text-width" :height 0.85 :v-adjust -0.05))
-;; ;; 	    (Method . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
-;; ;; 	    (Function . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
-;; ;; 	    (Constructor . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
-;; ;; 	    (Field . ,(all-the-icons-faicon "tag" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-lblue))
-;; ;; 	    (Variable . ,(all-the-icons-faicon "tag" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-lblue))
-;; ;; 	    (Class . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
-;; ;; 	    (Interface . ,(all-the-icons-material "share" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
-;; ;; 	    (Module . ,(all-the-icons-material "view_module" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
-;; ;; 	    (Property . ,(all-the-icons-faicon "wrench" :height 0.85 :v-adjust -0.05))
-;; ;; 	    (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (Value . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
-;; ;; 	    (Enum . ,(all-the-icons-material "storage" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
-;; ;; 	    (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (Snippet . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (Color . ,(all-the-icons-material "palette" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (File . ,(all-the-icons-faicon "file-o" :height 0.9 :v-adjust -0.05))
-;; ;; 	    (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (Folder . ,(all-the-icons-faicon "folder-open" :height 0.9 :v-adjust -0.05))
-;; ;; 	    (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
-;; ;; 	    (Constant . ,(all-the-icons-faicon "square-o" :height 0.9 :v-adjust -0.05))
-;; ;; 	    (Struct . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
-;; ;; 	    (Event . ,(all-the-icons-faicon "bolt" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-orange))
-;; ;; 	    (Operator . ,(all-the-icons-material "control_point" :height 0.9 :v-adjust -0.2))
-;; ;; 	    (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.85 :v-adjust -0.05))
-;; ;; 	    (Template . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2)))
-;; ;; 	  company-box-icons-alist 'company-box-icons-all-the-icons))
-;; ;; 
-;; ;;   )
-;; 
-;; 
-;; ;; (use-package solaire-mode
-;; ;;   :hook
-;; ;;   ( ;((change-major-mode after-revert ediff-prepare-buffer) . 'turn-on-solaire-mode)
-;; ;;    (minibuffer-setup . solaire-mode-in-minibuffer))
-;; ;; ;;   :config
-;; ;; ;; 
-;; ;; ;;   ;; solaire-mode messes with hl-line-mode when using the daemon
-;; ;; ;;   (defun siliusmv/add-solaire (frame)
-;; ;; ;;     (select-frame frame)
-;; ;; ;;     (if (display-graphic-p)
-;; ;; ;; 	(if (not (bound-and-true-p solaire-mode))
-;; ;; ;; 	    (progn
-;; ;; ;; 	      (solaire-global-mode)
-;; ;; ;; 	      (solaire-mode-swap-bg) ; Only necessary for doom-themes
-;; ;; ;; 	      ))
-;; ;; ;;       ;; This part is for when you open a non-graphical frame and everything
-;; ;; ;;       ;; gets really ugly
-;; ;; ;;       (if siliusmv/is-dark-theme
-;; ;; ;; 	  (load-theme 'doom-one t)
-;; ;; ;; 	(load-theme 'doom-one-light t))))
-;; ;; ;; 
-;; ;; ;;   (if (daemonp)
-;; ;; ;;       (add-hook 'after-make-frame-functions #'siliusmv/add-solaire)
-;; ;; ;;     (solaire-global-mode)
-;; ;; ;;     (solaire-mode-swap-bg))
-;; ;; 
-;; ;;   )
 
 
 ;; =========================================================
